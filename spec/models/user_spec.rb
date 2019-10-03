cd05ca57860d068686878030d4d96c0334adc815
ethudev <anh.tq2411ps@gmail.com>
2019-10-03 09:12:34 +0000

    Installed rspec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+   subject { described_class.new(first_name: "Johny",
+    last_name: "Cage")
+  }
+  
+  it "is valid with valid attributes" do
+    expect(subject).to be_valid 
+  end
+  
 end

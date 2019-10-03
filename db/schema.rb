b2333a826a30e013e040d31bcaf3f5a44f09710d
ethudev <anh.tq2411ps@gmail.com>
2019-10-03 09:33:14 +0000

    Added model test for game

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_03_090113) do
+ActiveRecord::Schema.define(version: 2019_10_03_092828) do
+
+  create_table "games", force: :cascade do |t|
+    t.integer "score"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe Game, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe User, type: :model do
+   subject { described_class.new(score: 8)}
+  
+  it "is valid with valid attributes" do
+    expect(subject).to be_valid 
+  end
+  
+  it "is not valid without score" do
+    subject.score = nil
+    expect(subject).not_to be_valid 
+  end
+  
+  
+  
 end

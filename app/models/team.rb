7b1c97f8360744f91734e3c07d60aa097b27acec
ethudev <anh.tq2411ps@gmail.com>
2019-10-05 16:01:10 +0000

    Implemented winrate method for team

--- app/models/team.rb
+++ app/models/team.rb
@@ -3,15 +3,18 @@ class Team < ApplicationRecord
   has_many :games, dependent: :destroy
   has_many :matchs, :through => :games
   
-  
   def winrate
-    total_matchs      = self.matchs.distinct
-    win_match   = 0
+    #Initialize matchs for current team
+    matchs       = self.matchs.distinct
+    total_matchs = self.matchs.distinct.count
+    win_match    = 0
     
-    total_matchs.each do  |match|
-      win_match += 1 if match.winner = self
+    #Iterate through all matchs, increase win_match if winner is "self"
+    matchs.each do  |match|
+      win_match += 1 if match.winner == self
     end
-    
+    #Rule out zero divider exception, convert result to float rounded to 3
+    total_matchs == 0 ? 0.0 : (win_match.to_f/total_matchs).round(3)
   end
   
 end

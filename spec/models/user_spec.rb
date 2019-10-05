41b4f475b6a33415a6fcd1b967f056c9fb9ac49f
ethudev <anh.tq2411ps@gmail.com>
2019-10-05 16:19:58 +0000

    Added test for user full name

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -19,7 +19,12 @@ RSpec.describe User, type: :model do
     subject.last_name = nil
     expect(subject).not_to be_valid 
   end
-
+  
+  it "can show full name" do 
+    expect(subject.full_name).not_to be_nil
+    expect(subject.full_name).to equal("Johny Cage")
+  end
+  
   #TODO Add scope for full name?
   
   

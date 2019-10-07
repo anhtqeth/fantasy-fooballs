f470718b14d582ae2636039a4665824440246cb8
ethudev <anh.tq2411ps@gmail.com>
2019-10-07 08:26:20 +0000

    Smooth out Matches controller. Not allow same team match creation

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -6,21 +6,25 @@ class MatchesController < ApplicationController
   end
   
   def create
-    #TODO - Prevent same team match
     @match = Match.new
-    teamA_params[:scores].each do |k, v|
-      Game.create(score: v,team: Team.find(teamA_params[:id]), match: @match)
-    end
-    
-    teamB_params[:scores].each do |k, v|
-      Game.create(score: v,team: Team.find(teamB_params[:id]), match: @match)
-    end
-    
-    
-    if @match.save
-      redirect_back(fallback_location: matches_path)
-    else
+    #This is a technical debt
+    #If both team id are the same >> not save the match
+    unless teamA_params[:id] == teamB_params[:id]
+      teamA_params[:scores].each do |k, v|
+        Game.create(score: v,team: Team.find(teamA_params[:id]), match: @match)
+      end
+      teamB_params[:scores].each do |k, v|
+        Game.create(score: v,team: Team.find(teamB_params[:id]), match: @match)
+      end
       
+      if @match.save
+        redirect_back(fallback_location: matches_path)
+      else
+        
+      end
+    else
+      flash[:danger] = "Same Team cannot fight each other!"
+      redirect_back(fallback_location: matches_path)
     end
   end
   
